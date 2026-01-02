51e8b798c194cddcf122760eb7ca717bd7631ee6
SamyrOR <saamyr@live.com>
2026-01-01 23:09:53 -0300

    feat(infra): added packages and db context

---
+++ infra/MyFinanceDBContext.cs
@@ -0,0 +1,13 @@
+﻿namespace infra;
+using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+public class MyFinanceDBContext : DbContext
+{
+    public DbSet<PlanoConta> PlanoCOnta {get; set;}
+    public DbSet<Transacao> Transacao {get; set;}
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Database=myfinance;Username=postgres;Password=yourpassword");
+    }
+}
