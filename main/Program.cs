ee23ad5a384d50bf133f9cf8fdab86877eef2efd
SamyrOR <saamyr@live.com>
2026-01-01 23:25:10 -0300

    feat(main): added dbcontext

--- main/Program.cs
+++ main/Program.cs
@@ -1,7 +1,11 @@
+using infra;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MyFinanceDBContext>();
 
 var app = builder.Build();
 
